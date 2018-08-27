55159e3bcb4122915b474e4a156bbb0e62970d4e
vibhor <vibhor@ziggletech.com>
2018-08-27 15:02:27 +0530

    first commit

---
+++ application/config/sq_config.php
@@ -0,0 +1,7 @@
+<?php 
+		$sq_base_url='http://localhost/savsoftquiz_v4.0_advance-master/';
+		$sq_hostname='localhost';
+		$sq_dbname='online-exam';
+		$sq_dbusername='root';
+		$sq_dbpassword='';
+		?>
---
+++ application/logs/log-2018-08-27.php
@@ -0,0 +1,69 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-08-27 13:20:27 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): No connection could be made because the target machine actively refused it.
+ C:\xampp\htdocs\savsoftquiz_v4.0_advance-master\system\database\drivers\mysqli\mysqli_driver.php 208
+ERROR - 2018-08-27 13:20:27 --> Unable to connect to the database
+ERROR - 2018-08-27 13:20:29 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): No connection could be made because the target machine actively refused it.
+ C:\xampp\htdocs\savsoftquiz_v4.0_advance-master\system\database\drivers\mysqli\mysqli_driver.php 208
+ERROR - 2018-08-27 13:20:29 --> Unable to connect to the database
+ERROR - 2018-08-27 13:20:29 --> Query error: No connection could be made because the target machine actively refused it.
+ - Invalid query: SELECT *
+FROM `savsoft_quiz`
+ORDER BY `quid` DESC
+ LIMIT 5
+ERROR - 2018-08-27 13:20:29 --> Severity: Error --> Call to a member function result_array() on boolean C:\xampp\htdocs\savsoftquiz_v4.0_advance-master\application\models\Quiz_model.php 37
+ERROR - 2018-08-27 13:20:42 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'sqtry2' C:\xampp\htdocs\savsoftquiz_v4.0_advance-master\system\database\drivers\mysqli\mysqli_driver.php 208
+ERROR - 2018-08-27 13:20:42 --> Unable to connect to the database
+ERROR - 2018-08-27 13:20:42 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'sqtry2' C:\xampp\htdocs\savsoftquiz_v4.0_advance-master\system\database\drivers\mysqli\mysqli_driver.php 208
+ERROR - 2018-08-27 13:20:42 --> Unable to connect to the database
+ERROR - 2018-08-27 13:20:42 --> Query error: Unknown database 'sqtry2' - Invalid query: SELECT *
+FROM `savsoft_quiz`
+ORDER BY `quid` DESC
+ LIMIT 5
+ERROR - 2018-08-27 13:20:42 --> Severity: Error --> Call to a member function result_array() on boolean C:\xampp\htdocs\savsoftquiz_v4.0_advance-master\application\models\Quiz_model.php 37
+ERROR - 2018-08-27 13:21:09 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'sqtry2' C:\xampp\htdocs\savsoftquiz_v4.0_advance-master\system\database\drivers\mysqli\mysqli_driver.php 208
+ERROR - 2018-08-27 13:21:09 --> Unable to connect to the database
+ERROR - 2018-08-27 13:21:09 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'sqtry2' C:\xampp\htdocs\savsoftquiz_v4.0_advance-master\system\database\drivers\mysqli\mysqli_driver.php 208
+ERROR - 2018-08-27 13:21:10 --> Unable to connect to the database
+ERROR - 2018-08-27 13:21:10 --> Query error: Unknown database 'sqtry2' - Invalid query: SELECT *
+FROM `savsoft_quiz`
+ORDER BY `quid` DESC
+ LIMIT 5
+ERROR - 2018-08-27 13:21:10 --> Severity: Error --> Call to a member function result_array() on boolean C:\xampp\htdocs\savsoftquiz_v4.0_advance-master\application\models\Quiz_model.php 37
+ERROR - 2018-08-27 13:21:33 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'sqtry2' C:\xampp\htdocs\savsoftquiz_v4.0_advance-master\system\database\drivers\mysqli\mysqli_driver.php 208
+ERROR - 2018-08-27 13:21:33 --> Unable to connect to the database
+ERROR - 2018-08-27 13:21:33 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'sqtry2' C:\xampp\htdocs\savsoftquiz_v4.0_advance-master\system\database\drivers\mysqli\mysqli_driver.php 208
+ERROR - 2018-08-27 13:21:33 --> Unable to connect to the database
+ERROR - 2018-08-27 13:21:33 --> Query error: Unknown database 'sqtry2' - Invalid query: SELECT *
+FROM `savsoft_quiz`
+ORDER BY `quid` DESC
+ LIMIT 5
+ERROR - 2018-08-27 13:21:33 --> Severity: Error --> Call to a member function result_array() on boolean C:\xampp\htdocs\savsoftquiz_v4.0_advance-master\application\models\Quiz_model.php 37
+ERROR - 2018-08-27 13:37:14 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'sqtry2' C:\xampp\htdocs\savsoftquiz_v4.0_advance-master\system\database\drivers\mysqli\mysqli_driver.php 208
+ERROR - 2018-08-27 13:37:14 --> Unable to connect to the database
+ERROR - 2018-08-27 13:37:14 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'sqtry2' C:\xampp\htdocs\savsoftquiz_v4.0_advance-master\system\database\drivers\mysqli\mysqli_driver.php 208
+ERROR - 2018-08-27 13:37:14 --> Unable to connect to the database
+ERROR - 2018-08-27 13:37:14 --> Query error: Unknown database 'sqtry2' - Invalid query: SELECT *
+FROM `savsoft_quiz`
+ORDER BY `quid` DESC
+ LIMIT 5
+ERROR - 2018-08-27 13:37:14 --> Severity: Error --> Call to a member function result_array() on boolean C:\xampp\htdocs\savsoftquiz_v4.0_advance-master\application\models\Quiz_model.php 37
+ERROR - 2018-08-27 13:47:59 --> Severity: Warning --> Division by zero C:\xampp\htdocs\savsoftquiz_v4.0_advance-master\application\views\dashboard.php 231
+ERROR - 2018-08-27 13:51:26 --> Severity: Warning --> Division by zero C:\xampp\htdocs\savsoftquiz_v4.0_advance-master\application\views\dashboard.php 231
+ERROR - 2018-08-27 13:55:50 --> Severity: Warning --> Division by zero C:\xampp\htdocs\savsoftquiz_v4.0_advance-master\application\views\dashboard.php 231
+ERROR - 2018-08-27 13:59:18 --> Severity: Warning --> Division by zero C:\xampp\htdocs\savsoftquiz_v4.0_advance-master\application\views\dashboard.php 231
+ERROR - 2018-08-27 13:59:39 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\savsoftquiz_v4.0_advance-master\application\models\Quiz_model.php 729
+ERROR - 2018-08-27 14:01:28 --> Could not find the language line "hello"
+ERROR - 2018-08-27 14:01:28 --> Could not find the language line "user_id"
+ERROR - 2018-08-27 14:01:28 --> Could not find the language line "notattempted"
+ERROR - 2018-08-27 14:08:05 --> Could not find the language line "hello"
+ERROR - 2018-08-27 14:08:05 --> Could not find the language line "user_id"
+ERROR - 2018-08-27 14:08:05 --> Could not find the language line "notattempted"
+ERROR - 2018-08-27 14:09:46 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\savsoftquiz_v4.0_advance-master\application\models\Quiz_model.php 729
+ERROR - 2018-08-27 14:12:53 --> Severity: Warning --> Division by zero C:\xampp\htdocs\savsoftquiz_v4.0_advance-master\application\views\dashboard.php 231
+ERROR - 2018-08-27 14:13:05 --> Could not find the language line "hello"
+ERROR - 2018-08-27 14:13:05 --> Could not find the language line "user_id"
+ERROR - 2018-08-27 14:13:05 --> Could not find the language line "notattempted"
+ERROR - 2018-08-27 14:45:33 --> Could not find the language line "hello"
+ERROR - 2018-08-27 14:45:33 --> Could not find the language line "user_id"
+ERROR - 2018-08-27 14:45:33 --> Could not find the language line "notattempted"
